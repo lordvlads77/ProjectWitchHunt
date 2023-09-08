72898a24a4c6fb4baea214304e59c2fffe90dac5
Pollogamerz <tbarajasglez05@gmail.com>
2023-09-08 02:33:01 -0600

    avr

--- Assets/Script-scrollabe/ScrollableBackground.cs
+++ Assets/Script-scrollabe/ScrollableBackground.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class ScrollableBackground : MonoBehaviour
 {
+    [System.Serializable]
+    public class WeightedBackground
+    {
+        public GameObject background;
+        public float weight;
+    }
+
+    public List<WeightedBackground> weightedBackgrounds; // Lista de fondos ponderados
     public float scrollSpeed = 1.0f;
+
+    private GameObject currentBackground;
+
+    void Start()
+    {
+        // Inicialmente, elige un fondo aleatorio basado en pesos
+        currentBackground = ChooseRandomBackground();
+        currentBackground.SetActive(true);
+    }
 
     void Update()
     {
-        // Obtén la posición actual del fondo
-        Vector3 currentPosition = transform.position;
-
-        // Calcula la nueva posición desplazada
+        // Desplaza el fondo actual
+        Vector3 currentPosition = currentBackground.transform.position;
         float newPositionX = currentPosition.x - (scrollSpeed * Time.deltaTime);
+        currentBackground.transform.position = new Vector3(newPositionX, currentPosition.y, currentPosition.z);
+    }
 
-        // Aplica la nueva posición al fondo
-        transform.position = new Vector3(newPositionX, currentPosition.y, currentPosition.z);
+    GameObject ChooseRandomBackground()
+    {
+        // Calcula el peso total de todos los fondos
+        float totalWeight = 0f;
+        foreach (var weightedBackground in weightedBackgrounds)
+        {
+            totalWeight += weightedBackground.weight;
+        }
+
+        // Elige un número aleatorio entre 0 y el peso total
+        float randomValue = Random.Range(0f, totalWeight);
+
+        // Encuentra el fondo correspondiente al valor aleatorio
+        foreach (var weightedBackground in weightedBackgrounds)
+        {
+            if (randomValue < weightedBackground.weight)
+            {
+                return weightedBackground.background;
+            }
+            randomValue -= weightedBackground.weight;
+        }
+
+        // Por si acaso, devuelve el último fondo en caso de problemas
+        return weightedBackgrounds[weightedBackgrounds.Count - 1].background;
     }
 }
