a886d834ecc49c01b825cd6a6a274cc8fb37f366
T.Felipe <85629754+Pollogamerz@users.noreply.github.com>
2023-09-07 18:50:31 -0600

    avance

---
+++ Assets/Scripts/Producto.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Producto : MonoBehaviour
+{
+    [SerializeField]
+    private string nombre;
+    private int precio;
+    [SerializeField]
+    private GameObject objeto;
+
+    public void Comprar()
+    {
+        //Para cuando se hagan los power ups
+    }
+}
