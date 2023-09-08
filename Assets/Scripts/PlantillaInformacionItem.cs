ac6d94aef91741ffe23890c20a949f49928747bd
Pollogamerz <tbarajasglez05@gmail.com>
2023-09-08 00:39:14 -0600

    basetienda

---
+++ Assets/Scripts/PlantillaInformacionItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="MenuTienda", menuName ="Plantilla/InfoItemTienda")]
+public class PlantillaInformacionItem : ScriptableObject
+{
+    [SerializeField]
+}
