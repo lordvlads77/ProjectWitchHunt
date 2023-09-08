d6e562d9f3d9ba8135fcf4acbb1be887a5c91530
Pollogamerz <tbarajasglez05@gmail.com>
2023-09-08 10:11:53 -0600

    sevemejor

--- Assets/Script-scrollabe/ScrollableBackground.cs
+++ Assets/Script-scrollabe/ScrollableBackground.cs
@@ -4,56 +4,25 @@ using UnityEngine;
 
 public class ScrollableBackground : MonoBehaviour
 {
-    [System.Serializable]
-    public class WeightedBackground
-    {
-        public GameObject background;
-        public float weight;
-    }
-
-    public List<WeightedBackground> weightedBackgrounds; // Lista de fondos ponderados
-    public float scrollSpeed = 1.0f;
-
-    private GameObject currentBackground;
-
-    void Start()
-    {
-        // Inicialmente, elige un fondo aleatorio basado en pesos
-        currentBackground = ChooseRandomBackground();
-        currentBackground.SetActive(true);
-    }
-
+    [SerializeField]
+    private float scrollSpeed = 1.0f;
+    [SerializeField]
+    private bool scrollHorizontally = true;
     void Update()
     {
-        // Desplaza el fondo actual
-        Vector3 currentPosition = currentBackground.transform.position;
-        float newPositionX = currentPosition.x - (scrollSpeed * Time.deltaTime);
-        currentBackground.transform.position = new Vector3(newPositionX, currentPosition.y, currentPosition.z);
-    }
+        Vector2 offset = GetComponent<Renderer>().material.mainTextureOffset;
 
-    GameObject ChooseRandomBackground()
-    {
-        // Calcula el peso total de todos los fondos
-        float totalWeight = 0f;
-        foreach (var weightedBackground in weightedBackgrounds)
+        float scrollAmount = Time.deltaTime * scrollSpeed;
+
+        if (scrollHorizontally)
         {
-            totalWeight += weightedBackground.weight;
+            offset.x += scrollAmount;
+        }
+        else
+        {
+            offset.y += scrollAmount;
         }
 
-        // Elige un número aleatorio entre 0 y el peso total
-        float randomValue = Random.Range(0f, totalWeight);
-
-        // Encuentra el fondo correspondiente al valor aleatorio
-        foreach (var weightedBackground in weightedBackgrounds)
-        {
-            if (randomValue < weightedBackground.weight)
-            {
-                return weightedBackground.background;
-            }
-            randomValue -= weightedBackground.weight;
-        }
-
-        // Por si acaso, devuelve el último fondo en caso de problemas
-        return weightedBackgrounds[weightedBackgrounds.Count - 1].background;
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
     }
 }
