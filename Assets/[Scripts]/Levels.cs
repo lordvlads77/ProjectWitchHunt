aca63d9d050688f9104d693f0c219182fecf7136
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-09-13 02:22:01 -0600

    New Script for Spawner WIP

---
+++ Assets/[Scripts]/Levels.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public class Levels
+{
+    public GameObject[] maps;
+}
---
+++ Assets/[Scripts]/spawnLevels.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnLevels : MonoBehaviour
+{
+    private Queue<GameObject> _levels;
+    void Start()
+    {
+        _levels = new Queue<GameObject>();
+    }
+    
+    public void StartLevel(GameObject gamelevel)
+    {
+        _levels.Clear();
+        foreach (GameObject _level in Levels.maps)
+        {
+            _levels.Enqueue(_level);
+        }
+        {
+            
+        }
+    } 
+}
