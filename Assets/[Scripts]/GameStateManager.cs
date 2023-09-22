646596d54f0ac67246972a8160c624c28779b64e
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-09-22 02:05:20 -0600

    Finish PauseSys

--- Assets/[Scripts]/GameStateManager.cs
+++ Assets/[Scripts]/GameStateManager.cs
@@ -18,7 +18,23 @@ public class GameStateManager
             return _instance;
         }
     }
-
+    
+    public GameState CurrentGameState { get; private set; }
     public delegate void GameStateChangeHandler(GameState newGameState);
     public event GameStateChangeHandler OnGameStateChanged;
+    
+    private GameStateManager()
+    {
+        
+    }
+    
+    public void SetState(GameState newGameState)
+    {
+        if (newGameState == CurrentGameState)
+        {
+            return;
+        }
+        CurrentGameState = newGameState;
+        OnGameStateChanged?.Invoke(newGameState);
+    }
 }
