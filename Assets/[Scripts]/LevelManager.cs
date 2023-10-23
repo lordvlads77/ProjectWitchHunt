3028136038733d5d30598f06bd9bf0724e5102f1
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-10-23 02:52:19 -0600

    Finish Spawn System | Good looks pending

--- Assets/[Scripts]/LevelManager.cs
+++ Assets/[Scripts]/LevelManager.cs
@@ -9,10 +9,13 @@ public class LevelManager : MonoBehaviour
 {
     public List<GameObject> availableLevels = new List<GameObject>();
     private List<GameObject> instantiatedLevels = new List<GameObject>();
+    private int currentIndex = -1;
 
     public Transform spawnPoint; // The position where levels will be instantiated
-
     private GameObject currentLevel; // The level that the player is currently on
+    private GameObject previousLevel; // The level that the player was previously on
+    
+    [Header("Player Pos Reset")]
     [FormerlySerializedAs("ogPosition")] public Vector3 ogPlayerPosition;
     public Transform playerPosition;
 
@@ -23,17 +26,55 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
-        // Ensure there are available levels to instantiate
         if (availableLevels.Count == 0)
         {
             Debug.LogError("No available levels to instantiate. Add levels to the 'availableLevels' list in the Inspector.");
             return;
         }
 
-        // Instantiate the first random level
-        InstantiateRandomLevel();
-    }
+        // Initialize by activating the first level
+        if (currentIndex == -1)
+        {
+            InstantiateRandomLevel1();
+        }
+    } 
+    
+    private void InstantiateRandomLevel1()
+    {
+        // Check if we've used all available levels
+        if (availableLevels.Count == 0)
+        {
+            Debug.LogWarning("All available levels have been used. Resetting the list.");
+            availableLevels.AddRange(instantiatedLevels);
+            instantiatedLevels.Clear();
+            currentIndex = -1; // Reset current level index
+        }
 
+        // Increment the current level index
+        currentIndex = (currentIndex + 1) % availableLevels.Count;
+
+        // Activate the next level
+        if (currentIndex >= 0 && currentIndex < instantiatedLevels.Count)
+        {
+            instantiatedLevels[currentIndex].SetActive(true);
+        }
+        else
+        {
+            // Instantiate the selected level at the spawnPoint
+            GameObject randomLevelPrefab = availableLevels[currentIndex];
+            GameObject instantiatedLevel = Instantiate(randomLevelPrefab, spawnPoint.position, randomLevelPrefab.transform.rotation);
+
+            // Add the instantiated level to the list of instantiated levels
+            instantiatedLevels.Add(instantiatedLevel);
+        }
+
+        // Deactivate the previously activated level
+        if (currentIndex > 0)
+        {
+            instantiatedLevels[currentIndex - 1].SetActive(false);
+        }
+    }
+    
     private void InstantiateRandomLevel()
     {
         // Check if we've used all available levels
@@ -48,14 +89,13 @@ public class LevelManager : MonoBehaviour
         int randomIndex = Random.Range(0, availableLevels.Count);
         GameObject randomLevelPrefab = availableLevels[randomIndex];
 
+        // Instantiate the selected level at the spawnPoint
         if (currentLevel != null)
         {
-            currentLevel.SetActive(false);
+            currentLevel.SetActive(false); // Deactivate the previous level
         }
 
-        // Instantiate the selected level at the spawnPoint
         currentLevel = Instantiate(randomLevelPrefab, spawnPoint.position, randomLevelPrefab.transform.rotation);
-        //GameObject instantiatedLevel = Instantiate(randomLevelPrefab, spawnPoint.position, randomLevelPrefab.transform.rotation);
 
         // Remove the selected level from availableLevels
         availableLevels.RemoveAt(randomIndex);
@@ -67,7 +107,12 @@ public class LevelManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Player entered the trigger");
-        InstantiateRandomLevel();
+        InstantiateRandomLevel1();
         playerPosition.transform.position = ogPlayerPosition;
+        if (instantiatedLevels[5].activeInHierarchy)
+        {
+            instantiatedLevels[5].SetActive(false);
+            InstantiateRandomLevel1();
+        }
     }
 }
