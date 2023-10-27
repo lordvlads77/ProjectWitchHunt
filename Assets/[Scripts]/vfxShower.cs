c5914d4798eb612f3de62633a74e025318d0c2d6
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-10-27 02:35:57 -0600

    Finish Script to call the Particles in the Sys needed

---
+++ Assets/[Scripts]/vfxShower.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vfxShower : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ParticleController.Instance.SpwnAttckParticle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ParticleController.Instance.SpwnDmgParticle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ParticleController.Instance.SpwnDmgAoeParticle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            ParticleController.Instance.SpwnProyectileBlueParticle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            ParticleController.Instance.SpwnProyectilePinkParticle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            ParticleController.Instance.SpwnLightningParticle();
+        }
+    }
+}
