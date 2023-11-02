572c87536ba869edbaacde162fbae5b99659ddc5
Pollogamerz <tbarajasglez05@gmail.com>
2023-11-02 03:07:15 -0600

    porahoratambien

--- Assets/Autoapuntado/DisparoAutomatico.cs
+++ Assets/Autoapuntado/DisparoAutomatico.cs
@@ -6,7 +6,7 @@ public class DisparoAutomatico : MonoBehaviour
     public GameObject proyectil;
     public float velocidadDisparo = 5f;
     public float frecuenciaDisparo = 1.5f;
-    public LayerMask capaEnemigo;
+    public string tagEnemigo = "Enemigo";
 
     private void Update()
     {
@@ -19,9 +19,12 @@ public class DisparoAutomatico : MonoBehaviour
     bool DetectarEnemigo()
     {
         RaycastHit hit;
-        if (Physics.Raycast(puntoDisparo.position, puntoDisparo.forward, out hit, Mathf.Infinity, capaEnemigo))
+        if (Physics.Raycast(puntoDisparo.position, puntoDisparo.forward, out hit, Mathf.Infinity))
         {
-            return true; // Hay un enemigo en la línea de visión
+            if (hit.collider.CompareTag(tagEnemigo))
+            {
+                return true; // Hay un objeto con la etiqueta de enemigo en la línea de visión
+            }
         }
         return false;
     }
